85b63079a0934e777c9a48145fdca3093a8055b7
Nitesh Kumar Napit <ntshnpt@gmail.com>
2021-07-27 09:41:32 +0530

    random user generator full project

--- app.js
+++ app.js
@@ -6,8 +6,29 @@ const app = Vue.createApp({
             lastName : 'Doe',
             email : 'john@gmail.com',
             gender : 'male',
-            picture : '',
+            picture : 'https://randomuser.me/api/portraits/men/10.jpg',
         }
+    },
+
+    methods: {
+        // getUser(){
+        //     console.log(this.firstName)   
+        //     this.firstName = 'Smith',
+        //     this.lastName = 'Daa',
+        //     this.email = 'smith@gmail.com',
+        //     this.gender = 'female',
+        //     this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+        // }
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+            console.log(results)
+            this.firstName = results[0].name.first,
+            this.lastName = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.picture = results[0].picture.large
+        },
     },
 })
 
